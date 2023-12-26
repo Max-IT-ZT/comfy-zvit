fe1326af59e9046f9afe78ea198308fd14893d9d
Max-IT-ZT <babuskomax@gmail.com>
2023-12-26 22:48:47 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,7 @@
 // ? Імпорт компонентів
 import { Profile } from './Profile/Profile';
-import { FriendList } from './FriendList/FriendList';
-import { Statistics } from './Statistics/Statistics';
-import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 // ? Виклик json файлів
-import transactions from '../data/transactions.json';
-import friends from '../data/friends.json';
 import user from '../data/user.json';
-import data from '../data/data.json';
 
 // ? Виклик віртуального DOM дерева
 export const App = () => {
@@ -21,16 +15,6 @@ export const App = () => {
           avatar={user.avatar}
           stats={user.stats}
         />
-      </div>
-
-      <div className="container">
-        <Statistics title="Upload stats" stats={data} />
-      </div>
-      <div className="container">
-        <FriendList friends={friends} />
-      </div>
-      <div className="container">
-        <TransactionHistory items={transactions} />
       </div>
     </section>
   );
