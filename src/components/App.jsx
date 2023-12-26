4a66e7b4b61a4aa2fcd22008834bf020443ea0ee
Max-IT-ZT <babuskomax@gmail.com>
2023-12-26 23:36:48 +0200

    First-try

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,67 @@
-// ? Імпорт компонентів
-import { Profile } from './Profile/Profile';
-// ? Виклик json файлів
-import user from '../data/user.json';
+// src/components/App.jsx
+import React, { useState } from 'react';
+import itPlanData from './ItPlanData'; // змінили шлях
+import hsPlanData from './HsPlanData'; // змінили шлях
+import getCurrentDay from './Utils'; // змінили шлях
 
-// ? Виклик віртуального DOM дерева
-export const App = () => {
+const App = () => {
+  const [itSum, setItSum] = useState('');
+  const [itShare, setItShare] = useState('');
+  const [happySum, setHappySum] = useState('');
+  const [happyShare, setHappyShare] = useState('');
+
+  const handleSubmit = () => {
+    const currentDay = getCurrentDay();
+
+    const itFact = parseInt(itSum, 10);
+    const itPlan = itPlanData[currentDay - 1].plan;
+    const itDeviation = ((itFact - itPlan) / itPlan) * 100;
+
+    const happyFact = parseInt(happySum, 10);
+    const happyPlan = hsPlanData[currentDay - 1].plan;
+    const happyDeviation = ((happyFact - happyPlan) / happyPlan) * 100;
+
+    const message = `Житомир
+      ІТ ${itPlan}/${itFact}(${itDeviation.toFixed(1)}%)
+      ${itShare}%
+      ХС ${happyPlan}/${happyFact}(${happyDeviation.toFixed(1)}%)
+      ${happyShare}%
+      Смарт 59
+      Ноут 8
+      ТВ 13`;
+
+    alert(message);
+  };
+
   return (
-    <section className="section">
-      <div className="container">
-        <Profile
-          username={user.username}
-          tag={user.tag}
-          location={user.location}
-          avatar={user.avatar}
-        />
-      </div>
-    </section>
+    <div>
+      <input
+        type="text"
+        placeholder="Сума за ІТ"
+        value={itSum}
+        onChange={e => setItSum(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Доля за ІТ (%)"
+        value={itShare}
+        onChange={e => setItShare(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Сума за Хеппі"
+        value={happySum}
+        onChange={e => setHappySum(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Доля за Хеппі (%)"
+        value={happyShare}
+        onChange={e => setHappyShare(e.target.value)}
+      />
+      <button onClick={handleSubmit}>Відправити</button>
+    </div>
   );
 };
+
+export default App;
